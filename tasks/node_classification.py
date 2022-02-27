11a821ca70e1bc6b33635c2c9a7eca657d311aeb
Stanley <shengzeang@live.com>
2022-02-27 14:58:34 +0800

    update NodeClassification

--- tasks/node_classification.py
+++ tasks/node_classification.py
@@ -1,12 +1,70 @@
+import time
+from torch.optim import Adam
+import torch.nn as nn
+
 from tasks.base_task import BaseTask
+from tasks.utils import accuracy, set_seed
 
 
 class NodeClassification(BaseTask):
-    def __init__(self):
+    def __init__(self, data, model, lr, weight_decay, epochs, device, loss_fn=nn.CrossEntropyLoss(), seed=42):
         super(NodeClassification, self).__init__()
+        if len(data) != 4:
+            raise ValueError("The fist parameter must be a tuple (labels, idx_train, idx_val, idx_test)!")
+
+        (self.__labels, self.__idx_train, self.__idx_val, self.__idx_test) = data
+        self.__model = model
+        self.__optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+        self.__epochs = epochs
+        self.__loss_fn = loss_fn
+        self.__device = device
+        self.__seed = seed
+
+        self.__call__()
+
+    def __call__(self):
+        set_seed(self.__seed)
+        self.__model = self.__model.to(self.__device)
+        self.__labels = self.__labels.to(self.__device)
+
+        t_total = time.time()
+        best_val = 0.
+        best_test = 0.
+        for epoch in range(self.__epochs):
+            t = time.time()
+            loss_train, acc_train = self._train()
+            acc_val, acc_test = self._evaluate()
+            print('Epoch: {:03d}'.format(epoch + 1),
+                  'loss_train: {:.4f}'.format(loss_train),
+                  'acc_train: {:.4f}'.format(acc_train),
+                  'acc_val: {:.4f}'.format(acc_val),
+                  'acc_test: {:.4f}'.format(acc_test),
+                  'time: {:.4f}s'.format(time.time() - t))
+            if acc_val > best_val:
+                best_val = acc_val
+                best_test = acc_test
+
+        print("Optimization Finished!")
+        print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+        print(f'Best val: {best_val:.4f}, best test: {best_test:.4f}')
+        return best_test
 
     def _evaluate(self):
-        pass
+        self.__model.eval()
+        output = self.__model.train_model(self.__device)
+
+        acc_val = accuracy(output[self.__idx_val], self.__labels[self.__idx_val])
+        acc_test = accuracy(output[self.__idx_test], self.__labels[self.__idx_test])
+        return acc_val, acc_test
 
     def _train(self):
-        pass
+        self.__model.train()
+        self.__optimizer.zero_grad()
+
+        output = self.__model.train_model(self.__device)
+        loss_train = self.__loss_fn(output[self.__idx_train], self.__labels[self.__idx_train])
+        acc_train = accuracy(output[self.__idx_train], self.__labels[self.__idx_train])
+        loss_train.backward()
+        self.__optimizer.step()
+
+        return loss_train.item(), acc_train
--- tasks/utils.py
+++ tasks/utils.py
@@ -1,33 +1,15 @@
-import time
-import torch.nn.functional as F
+import torch
+import numpy as np
 
 
 def accuracy(output, labels):
     pred = output.max(1)[1].type_as(labels)
     correct = pred.eq(labels).double()
     correct = correct.sum()
-    return correct / len(labels)
+    return (correct / len(labels)).item()
 
 
-def train(epoch, model, optimizer, labels, idx_train, idx_val, idx_test, device):
-    t = time.time()
-    model.train()
-    optimizer.zero_grad()
-    output = model.train_model(device)
-    loss_train = F.cross_entropy(output[idx_train], labels[idx_train])
-    acc_train = accuracy(output[idx_train], labels[idx_train])
-    loss_train.backward()
-    optimizer.step()
-
-    model.eval()
-    output = model.train_model(device)
-
-    acc_val = accuracy(output[idx_val], labels[idx_val])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
-    print('Epoch: {:03d}'.format(epoch + 1),
-          'loss_train: {:.4f}'.format(loss_train.item()),
-          'acc_train: {:.4f}'.format(acc_train.item()),
-          'acc_val: {:.4f}'.format(acc_val.item()),
-          'acc_test: {:.4f}'.format(acc_test.item()),
-          'time: {:.4f}s'.format(time.time() - t))
-    return acc_val, acc_test
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
