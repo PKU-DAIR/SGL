ab6ad1141ddf8c01ad39f54f37003eb8b50c3df2
Stanley <shengzeang@live.com>
2022-03-09 18:15:43 +0800

    minor fixes

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ dataset = Planetoid("pubmed", "./", "official")
 model = SGC(prop_steps=3, feat_dim=dataset.num_features, num_classes=dataset.num_classes)
 
 device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
-cora_test = NodeClassification(dataset, model, lr=0.1, weight_decay=5e-4, epochs=200, device=device)
+cora_test = NodeClassification(dataset, model, lr=0.1, weight_decay=5e-5, epochs=200, device=device)
