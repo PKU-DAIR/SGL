6bd76a012b4fdba07a6ea33dc7b84e2f4e53a2fd
Tony Cao <137789103@qq.com>
2022-03-25 16:49:14 +0800

    format code

--- dataset/reddit.py
+++ dataset/reddit.py
@@ -14,6 +14,7 @@ from dataset.base_dataset import NodeDataset
 from dataset.utils import pkl_read_file, download_to
 from torch_geometric.data import extract_zip
 
+
 class Reddit(NodeDataset):
     def __init__(self, name="reddit", root="./", split="official"):
         if name not in ["reddit"]:
@@ -23,7 +24,8 @@ class Reddit(NodeDataset):
         self._data = pkl_read_file(self.processed_file_paths)
         self._split = split
         self._train_mask, self._val_mask, self._test_mask = None, None, None
-        self._train_idx, self._val_idx, self._test_idx = self.__generate_split(split)
+        self._train_idx, self._val_idx, self._test_idx = self.__generate_split(
+            split)
 
     @property
     def raw_file_paths(self):
@@ -34,7 +36,7 @@ class Reddit(NodeDataset):
     def processed_file_paths(self):
         filename = "graph"
         return osp.join(self._processed_dir, "{}.{}".format(self._name, filename))
-    
+
     def _download(self):
         url = "https://data.dgl.ai/dataset/reddit.zip"
         path = osp.join(self._raw_dir, "reddit.zip")
@@ -43,26 +45,28 @@ class Reddit(NodeDataset):
         os.unlink(path)
 
     def _process(self):
-        adj = sp.coo_matrix(sp.load_npz(osp.join(self._raw_dir, 'reddit_graph.npz')))
+        adj = sp.coo_matrix(sp.load_npz(
+            osp.join(self._raw_dir, 'reddit_graph.npz')))
     #    adj = adj[:100, :]
     #    print(f"adj shape: {adj.shape}")
         row, col, edge_weight = adj.row, adj.col, adj.data
         edge_type = "post__to__post"
 
         data = np.load(osp.join(self._raw_dir, 'reddit_data.npz'))
-        features, labels, split = data['feature'], data['label'], data['node_types']  
-    #    features = features[:100, :]   
+        features, labels, split = data['feature'], data['label'], data['node_types']
+    #    features = features[:100, :]
     #    print(f"feature shape: {features.shape}")
-        #后面需要修改，通过排序让train_data排在数据集最前面
+    # 后面需要修改，通过排序让train_data排在数据集最前面
         num_node = features.shape[0]
         node_type = "post"
         labels = torch.LongTensor(labels)
-        
+
         self._train_mask = split == 1
         self._val_mask = split == 2
         self._test_mask = split == 3
 
-        g = Graph(row, col, edge_weight, num_node, node_type, edge_type, x=features, y=labels)
+        g = Graph(row, col, edge_weight, num_node,
+                  node_type, edge_type, x=features, y=labels)
         with open(self.processed_file_paths, 'wb') as rf:
             try:
                 pkl.dump(g, rf)
@@ -79,10 +83,10 @@ class Reddit(NodeDataset):
                 self._train_mask = split == 1
                 self._val_mask = split == 2
                 self._test_mask = split == 3
-            
-            train_idx = np.where(self._train_mask==True)
-            val_idx = np.where(self._val_mask==True)
-            test_idx = np.where(self._test_mask==True)
+
+            train_idx = np.where(self._train_mask == True)
+            val_idx = np.where(self._val_mask == True)
+            test_idx = np.where(self._test_mask == True)
         elif split == "random":
             raise NotImplementedError
         else:
