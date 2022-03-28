67e0df0aeb1ebfa730fd4e886617adbbb8fc61af
Tony Cao <137789103@qq.com>
2022-03-28 22:48:45 +0800

    airports supported

---
+++ dataset/airports.py
@@ -0,0 +1,124 @@
+import os.path as osp
+import pickle as pkl
+from pyexpat import features
+
+import networkx as nx
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+from dataset.base_data import Graph
+from dataset.base_dataset import NodeDataset
+from dataset.utils import pkl_read_file, download_to
+
+
+class Airports(NodeDataset):
+    def __init__(self, name="usa", root="./", split="official", num_train_per_class=5, num_valid_per_class=5):
+        name = name.lower()
+        if name not in ["usa", "brazil", "europe"]:
+            raise ValueError("Dataset name not found!")
+        super(Airports, self).__init__(root + "Airports/", name)
+
+        self._data = pkl_read_file(self.processed_file_paths)
+        self._split = split
+        self._num_train_per_class = num_train_per_class
+        self._num_valid_per_class = num_valid_per_class
+        self._train_idx, self._val_idx, self._test_idx = self.__generate_split(
+            split)
+
+    @property
+    def raw_file_paths(self):
+        return [
+            osp.join(self._raw_dir, f'{self._name}-airports.edgelist'),
+            osp.join(self._raw_dir, f'labels-{self.name}-airports.txt')
+        ]
+
+    @property
+    def processed_file_paths(self):
+        return osp.join(self._processed_dir, f"{self._name}.graph")
+
+    def _download(self):
+        edge_url = ('https://github.com/leoribeiro/struc2vec/'
+                    'raw/master/graph/{}-airports.edgelist')
+        label_url = ('https://github.com/leoribeiro/struc2vec/'
+                     'raw/master/graph/labels-{}-airports.txt')
+
+        edge_url = edge_url.format(self._name)
+        label_url = label_url.format(self._name)
+
+        download_to(edge_url, self.raw_file_paths[0])
+        download_to(label_url, self.raw_file_paths[1])
+
+    def _process(self):
+        index_map, ys = {}, []
+        with open(self.raw_file_paths[1], 'r') as f:
+            data = f.read().split('\n')[1:-1]
+            for i, row in enumerate(data):
+                idx, y = row.split()
+                index_map[int(idx)] = i
+                ys.append(int(y))
+        labels = torch.LongTensor(ys)
+
+        # One-hot features
+        features = np.eye(labels.shape[0])
+        num_node = features.shape[0]
+        node_type = "airport"
+
+        edge_indices = []
+        with open(self.raw_file_paths[0], 'r') as f:
+            data = f.read().split('\n')[:-1]
+            for row in data:
+                src, dst = row.split()
+                edge_indices.append([index_map[int(src)], index_map[int(dst)]])
+        edge_index = np.array(edge_indices).T
+        row, col = edge_index[0], edge_index[1]
+
+        print("features: \n", features)
+        print("feature shape: ", features.shape)
+        print("num_node: ", num_node)
+        print("row shape: ", row.shape, "\ncol shape: ", col.shape)
+        print("labels shape: ", labels.shape)
+        print(edge_index.shape)
+
+        # Set default edge weight to 1
+        edge_weight = np.ones(len(row))
+        edge_type = "airport__to__airport"
+
+        g = Graph(row, col, edge_weight, num_node, node_type, edge_type, x=features, y=labels)
+        with open(self.processed_file_paths, 'wb') as rf:
+            try:
+                pkl.dump(g, rf)
+            except IOError as e:
+                print(e)
+                exit(1)
+
+    def __generate_split(self, split):
+        if split == "official":
+            index_map, ys = {}, []
+            with open(self.raw_file_paths[1], 'r') as f:
+                data = f.read().split('\n')[1:-1]
+                for i, row in enumerate(data):
+                    idx, y = row.split()
+                    index_map[int(idx)] = i
+                    ys.append(int(y))
+            labels = np.array(ys)
+
+            num_train_per_class = self._num_train_per_class
+            num_val = self._num_valid_per_class
+            train_idx, val_idx, test_idx = np.empty(0), np.empty(0), np.empty(0)
+            for i in range(self.num_classes):
+                idx = np.where(labels == i)[0]
+                train_idx = np.append(train_idx, idx[:num_train_per_class])
+                val_idx = np.append(val_idx, idx[num_train_per_class: num_train_per_class + num_val])
+                test_idx = np.append(test_idx, idx[num_train_per_class + num_val:])
+            train_idx.reshape(-1)
+            val_idx.reshape(-1)
+            test_idx.reshape(-1)
+
+
+        elif split == "random":
+            raise NotImplementedError
+        else:
+            raise ValueError("Please input valid split pattern!")
+
+        return train_idx, val_idx, test_idx
--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import torch
+from dataset.reddit import Reddit
 
 from models.SGAP_models import GAMLPRecursive, GAMLP, SGC, SIGN
 from dataset.planetoid import Planetoid
 from dataset.ogbn import Ogbn
 from tasks.node_classification import NodeClassification
+from dataset.ppi import PPI
+from dataset.facebook import Facebook
+from dataset.twitch import Twitch
+from dataset.karateclub import KarateClub
+from dataset.airports import Airports
 
-dataset = Planetoid("pubmed", "./", "official")
-# dataset = Ogbn("products", "./", "official")
+#dataset = Planetoid("cora", "./", "official")
+#dataset = Ogbn("products", "./", "official")
+dataset = Airports()
 
 model = SIGN(prop_steps=6, feat_dim=dataset.num_features, num_classes=dataset.num_classes, hidden_dim=64, num_layers=2)
 
 device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
-cora_test = NodeClassification(dataset, model, lr=0.1, weight_decay=5e-4, epochs=200, device=device)
+us_airports_test = NodeClassification(dataset, model, lr=0.1, weight_decay=5e-4, epochs=200, device=device)
