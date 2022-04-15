36b526f14a48eba45ba44730334bbe9d82cbddc1
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-04-15 17:06:58 +0800

    Update hetero_SGAP_models.py

--- models/hetero_SGAP_models.py
+++ models/hetero_SGAP_models.py
@@ -14,3 +14,19 @@ class NARS_SIGN(BaseHeteroSGAPModel):
 
         self._aggregator = OneDimConvolution(random_subgraph_num, prop_steps + 1, feat_dim)
         self._base_model = MultiLayerPerceptron(hidden_dim * (prop_steps + 1), hidden_dim, num_layers, num_classes)
+
+class NARS_SIGN_WeightSharedAcrossFeatures(BaseHeteroSGAPModel):
+    def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num,
+                 subgraph_edge_type_num):
+        super(NARS_SIGN_WeightSharedAcrossFeatures, self).__init__(prop_steps, feat_dim,
+                                        num_classes, random_subgraph_num, subgraph_edge_type_num)
+
+        self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
+        self._pre_msg_op = ProjectedConcatMessageOp(
+            0, prop_steps + 1, feat_dim, hidden_dim, num_layers)
+
+        self._aggregator = OneDimConvolutionWeightSharedAcrossFeatures(
+            random_subgraph_num, prop_steps + 1, feat_dim)
+        self._base_model = MultiLayerPerceptron(
+            hidden_dim * (prop_steps + 1), hidden_dim, num_layers, num_classes)
+        
