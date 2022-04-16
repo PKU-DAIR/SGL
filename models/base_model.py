a20035405d39d1203374e243c01e63e34d49b619
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-04-16 09:44:03 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -21,12 +22,14 @@ class BaseSGAPModel(nn.Module):
 
     def preprocess(self, adj, feature):
         if self._pre_graph_op is not None:
-            self._processed_feat_list = self._pre_graph_op.propagate(adj, feature)
+            self._processed_feat_list = self._pre_graph_op.propagate(
+                adj, feature)
             if self._pre_msg_op.aggr_type in ["proj_concat", "learnable_weighted", "iterate_learnable_weighted"]:
                 self._pre_msg_learnable = True
             else:
                 self._pre_msg_learnable = False
-                self._processed_feature = self._pre_msg_op.aggregate(self._processed_feat_list)
+                self._processed_feature = self._pre_msg_op.aggregate(
+                    self._processed_feat_list)
         else:
             self._pre_msg_learnable = False
             self._processed_feature = feature
@@ -35,7 +38,8 @@ class BaseSGAPModel(nn.Module):
     def postprocess(self, output):
         if self._post_graph_op is not None:
             if self._post_msg_op.aggr_type in ["proj_concat", "learnable_weighted", "iterate_learnable_weighted"]:
-                raise ValueError("Learnable weighted message operator is not supported in the post-processing phase!")
+                raise ValueError(
+                    "Learnable weighted message operator is not supported in the post-processing phase!")
             output = F.softmax(output, dim=1)
             output = self._post_msg_op(self._post_graph_op(output))
 
@@ -50,8 +54,10 @@ class BaseSGAPModel(nn.Module):
         if self._pre_msg_learnable is False:
             processed_feature = self._processed_feature[idx].to(device)
         else:
-            transferred_feat_list = [feat[idx].to(device) for feat in self._processed_feat_list]
-            processed_feature = self._pre_msg_op.aggregate(transferred_feat_list)
+            transferred_feat_list = [feat[idx].to(
+                device) for feat in self._processed_feat_list]
+            processed_feature = self._pre_msg_op.aggregate(
+                transferred_feat_list)
 
         output = self._base_model(processed_feature)
         return output
@@ -76,13 +82,14 @@ class BaseHeteroSGAPModel(nn.Module):
 
     def preprocess(self, dataset, predict_class):
         if not isinstance(dataset, HeteroNodeDataset):
-            raise TypeError("Dataset must be an instance of HeteroNodeDataset!")
+            raise TypeError(
+                "Dataset must be an instance of HeteroNodeDataset!")
         elif predict_class not in dataset.node_types:
             raise ValueError("Please input valid node class for prediction!")
         predict_idx = dataset.data.node_id_dict[predict_class]
 
         subgraph_dict, adopted_subgraph_num = dataset.nars_preprocess(dataset.edge_types, predict_class, self._random_subgraph_num,
-                                                self._subgraph_edge_type_num)
+                                                                      self._subgraph_edge_type_num)
         self._random_subgraph_num = adopted_subgraph_num
 
         self._propagated_feat_list_list = []
@@ -119,5 +126,78 @@ class BaseHeteroSGAPModel(nn.Module):
         aggregated_feat_list = self._aggregator(feat_input)
         combined_feat = self._pre_msg_op.aggregate(aggregated_feat_list)
         output = self._base_model(combined_feat)
+
+        return output
+
+
+class FastBaseHeteroSGAPModel(nn.Module):
+    def __init__(self, prop_steps, feat_dim, num_classes, random_subgraph_num, subgraph_edge_type_num):
+        super(FastBaseHeteroSGAPModel, self).__init__()
+        self._prop_steps = prop_steps
+        self._feat_dim = feat_dim
+        self._num_classes = num_classes
+        self._random_subgraph_num = random_subgraph_num
+        self._subgraph_edge_type_num = subgraph_edge_type_num
+
+        self._pre_graph_op = None
+        self._aggregator = None
+        self._base_model = None
+
+        self._propagated_feat_list_list = None
+        self._processed_feature_list = None
+        self._pre_msg_learnable = False
+
+    def preprocess(self, dataset, predict_class):
+        if not isinstance(dataset, HeteroNodeDataset):
+            raise TypeError(
+                "Dataset must be an instance of HeteroNodeDataset!")
+        elif predict_class not in dataset.node_types:
+            raise ValueError("Please input valid node class for prediction!")
+        predict_idx = dataset.data.node_id_dict[predict_class]
+
+        subgraph_dict, adopted_subgraph_num = dataset.nars_preprocess(dataset.edge_types, predict_class, self._random_subgraph_num,
+                                                                      self._subgraph_edge_type_num)
+        self._random_subgraph_num = adopted_subgraph_num
+
+        self._propagated_feat_list_list = []
+        for _ in range(self._prop_steps + 1):
+            self._propagated_feat_list_list.append([])
+        # subgraph = adj, feature, node_id
+        for key in subgraph_dict.keys():
+            edge_type_list = []
+            for edge_type in key:
+                edge_type_list.append(edge_type.split("__")[0])
+                edge_type_list.append(edge_type.split("__")[2])
+            if predict_class in edge_type_list:
+                adj, feature, node_id = subgraph_dict[key]
+                propagated_feature = self._pre_graph_op.propagate(adj, feature)
+
+                start_pos = list(node_id).index(predict_idx[0])
+                for i, feature in enumerate(propagated_feature):
+                    self._propagated_feat_list_list[i].append(
+                        feature[start_pos:start_pos + dataset.data.num_node[predict_class]])
+
+        # 2-d list to 4-d tensor (num_node, feat_dim, num_subgraphs, prop_steps)
+        self._propagated_feat_list_list = [torch.stack(
+            x, dim=2) for x in self._propagated_feat_list_list]
+        self._propagated_feat_list_list = torch.stack(
+            self._propagated_feat_list_list, dim=3)
+
+        # 4-d tensor to 3-d tensor (num_node, feat_dim, num_subgraphs * prop_steps)
+        shape = self._propagated_feat_list_list.size()
+        self._propagated_feat_list_list = self._propagated_feat_list_list.view(
+            shape[0], shape[1], shape[2]*shape[3])
+
+        return self._propagated_feat_list_list
+
+    # a wrapper of the forward function
+    def model_forward(self, idx, device):
+        return self.forward(idx, device)
+
+    def forward(self, idx, device):
+        feat_input = self._propagated_feat_list_list[idx].to(device)
+
+        aggregated_feat_from_diff_hops = self._aggregator(feat_input)
+        output = self._base_model(aggregated_feat_from_diff_hops)
 
         return output
