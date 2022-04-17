783912f327d79061e7675f5501a2d730b73c5a22
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-04-17 17:55:27 +0800

    Update stability_of_subgraph_weight.py

--- stability_of_subgraph_weight.py
+++ stability_of_subgraph_weight.py
@@ -1,5 +1,6 @@
 from auto_select_edge_type_for_nars import SMALL_NUM_EPOCHS, \
     GenerateSubgraphList, OneTrialWithSubgraphList
+from dataset.dblp import Dblp
 
 SUBGRAPH_COMBINATIONS = 20
 NUM_REPEAT = 5
@@ -7,14 +8,15 @@ SUBGRAPH_CONFIG = [(3, 2), (3, 3)]
 
 
 def main():
+    dataset = Dblp(root='.', path_of_zip='./dataset/DBLP_processed.zip')
     with open('subgraph_weight.txt', 'w') as output:
         for i in range(SUBGRAPH_COMBINATIONS):
             output.write(f'Subgraph combination {i+1}\n')
-            subgraph_list = GenerateSubgraphList(SUBGRAPH_CONFIG)
+            subgraph_list = GenerateSubgraphList(dataset, SUBGRAPH_CONFIG)
             for j in range(NUM_REPEAT):
                 output.write(f'\tIteration {j+1}\n')
                 test_acc, _, subgraph_weight = OneTrialWithSubgraphList(
-                    subgraph_list, SMALL_NUM_EPOCHS)
+                    dataset, subgraph_list, SMALL_NUM_EPOCHS)
                 output.write(f'\t\tTest accuracy: {test_acc}\n')
                 output.write(f'\t\tSubgraph weight: {subgraph_weight}\n\n')
                 output.flush()
