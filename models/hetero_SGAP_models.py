9817044c1ccd0a21c87d2a5eca75e0c1e816537d
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-04-18 07:27:40 +0800

    Update hetero_SGAP_models.py

--- models/hetero_SGAP_models.py
+++ models/hetero_SGAP_models.py
@@ -6,10 +6,8 @@ from models.simple_models import OneDimConvolution, MultiLayerPerceptron, \
 
 
 class NARS_SIGN(BaseHeteroSGAPModel):
-    def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num,
-                 subgraph_edge_type_num):
-        super(NARS_SIGN, self).__init__(prop_steps, feat_dim,
-                                        num_classes, random_subgraph_num, subgraph_edge_type_num)
+    def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num):
+        super(NARS_SIGN, self).__init__(prop_steps, feat_dim, num_classes)
 
         self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
         self._pre_msg_op = ProjectedConcatMessageOp(
@@ -19,39 +17,7 @@ class NARS_SIGN(BaseHeteroSGAPModel):
             random_subgraph_num, prop_steps + 1, feat_dim)
         self._base_model = MultiLayerPerceptron(
             hidden_dim * (prop_steps + 1), hidden_dim, num_layers, num_classes)
-
-
-class NARS_SIGN_WeightSharedAcrossFeatures(BaseHeteroSGAPModel):
-    def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num,
-                 subgraph_edge_type_num):
-        super(NARS_SIGN_WeightSharedAcrossFeatures, self).__init__(prop_steps, feat_dim,
-                                                                   num_classes, random_subgraph_num, subgraph_edge_type_num)
-
-        self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
-        self._pre_msg_op = ProjectedConcatMessageOp(
-            0, prop_steps + 1, feat_dim, hidden_dim, num_layers)
-
-        self._aggregator = OneDimConvolutionWeightSharedAcrossFeatures(
-            random_subgraph_num, prop_steps + 1)
-        self._base_model = MultiLayerPerceptron(
-            hidden_dim * (prop_steps + 1), hidden_dim, num_layers, num_classes)
-
-
-class NARS_SGC_WithLearnableWeights(BaseHeteroSGAPModel):
-    def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num,
-                 subgraph_edge_type_num):
-        super(NARS_SGC_WithLearnableWeights, self).__init__(prop_steps, feat_dim,
-                                                            num_classes, random_subgraph_num, subgraph_edge_type_num)
-
-        self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
-        self._pre_msg_op = LearnableWeightedMessageOp(
-            0, prop_steps + 1, 'simple_allow_neg', prop_steps)
-
-        self._aggregator = OneDimConvolutionWeightSharedAcrossFeatures(
-            random_subgraph_num, prop_steps + 1)
-        self._base_model = MultiLayerPerceptron(
-            feat_dim, hidden_dim, num_layers, num_classes)
-
+        
 
 class Fast_NARS_SGC_WithLearnableWeights(FastBaseHeteroSGAPModel):
     def __init__(self, prop_steps, feat_dim, num_classes, hidden_dim, num_layers, random_subgraph_num):
