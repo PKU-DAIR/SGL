92f706e025318ada6dc84143d597f79320ac8eda
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-04-17 17:38:13 +0800

    Update auto_select_edge_type_for_nars.py

--- auto_select_edge_type_for_nars.py
+++ auto_select_edge_type_for_nars.py
@@ -6,7 +6,8 @@ import warnings
 import torch
 
 from dataset.dblp import Dblp
-from models.hetero_SGAP_models import NARS_SIGN, NARS_SIGN_WeightSharedAcrossFeatures, NARS_SGC_WithLearnableWeights, Fast_NARS_SGC_WithLearnableWeights
+from models.hetero_SGAP_models import NARS_SIGN, NARS_SIGN_WeightSharedAcrossFeatures,\
+    NARS_SGC_WithLearnableWeights, Fast_NARS_SGC_WithLearnableWeights
 from tasks.node_classification import HeteroNodeClassification
 from auto_choose_gpu import GpuWithMaxFreeMem
 
@@ -20,11 +21,8 @@ LR = 0.01
 WEIGHT_DECAY = 0.0
 BATCH_SIZE = 10000
 
-dataset = Dblp(root='.', path_of_zip='./dataset/DBLP_processed.zip')
-predict_class = dataset.TYPE_OF_NODE_TO_PREDICT
 
-
-def GenerateSubgraphsWithSameEdgeTypeNum(random_subgraph_num: int, subgraph_edge_type_num: int) -> Dict:
+def GenerateSubgraphsWithSameEdgeTypeNum(dataset, random_subgraph_num: int, subgraph_edge_type_num: int) -> Dict:
     return dataset.nars_preprocess(edge_types=dataset.EDGE_TYPES,
                                    predict_class=dataset.TYPE_OF_NODE_TO_PREDICT,
                                    random_subgraph_num=random_subgraph_num,
@@ -33,9 +31,9 @@ def GenerateSubgraphsWithSameEdgeTypeNum(random_subgraph_num: int, subgraph_edge
 
 # Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
 # Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
-def GenerateSubgraphDict(subgraph_config: List) -> Dict:
+def GenerateSubgraphDict(dataset, subgraph_config: List) -> Dict:
     subgraph_list = [GenerateSubgraphsWithSameEdgeTypeNum(
-        random_subgraph_num, subgraph_edge_type_num)
+        dataset, random_subgraph_num, subgraph_edge_type_num)
         for random_subgraph_num, subgraph_edge_type_num
         in subgraph_config]
 
@@ -48,15 +46,17 @@ def Dict2List(dict: Dict) -> List:
 
 # Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
 # Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
-def GenerateSubgraphList(subgraph_config: List) -> List:
-    return Dict2List(GenerateSubgraphDict(subgraph_config))
+def GenerateSubgraphList(dataset, subgraph_config: List) -> List:
+    return Dict2List(GenerateSubgraphDict(dataset, subgraph_config))
 
 
 # Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
 # Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
-def OneTrialWithSubgraphConfig(subgraph_config: List, num_epochs: int) -> Tuple[
+def OneTrialWithSubgraphConfig(dataset, subgraph_config: List, num_epochs: int) -> Tuple[
         float, List, torch.torch.Tensor]:
     subgraph_list = GenerateSubgraphList(subgraph_config)
+
+    predict_class = dataset.TYPE_OF_NODE_TO_PREDICT
 
     model = Fast_NARS_SGC_WithLearnableWeights(prop_steps=PROP_STEPS,
                                                feat_dim=dataset.data.num_features[predict_class],
@@ -76,7 +76,6 @@ def OneTrialWithSubgraphConfig(subgraph_config: List, num_epochs: int) -> Tuple[
     raw_weight = classification.subgraph_weight
     weight_sum = raw_weight.sum()
     normalized_weight = raw_weight/weight_sum
-    print(normalized_weight)
 
     return test_acc, subgraph_list, normalized_weight
 
@@ -85,29 +84,39 @@ def TopKIndex(k: int, tensor: torch.Tensor) -> List:
     return heapq.nlargest(k, range(tensor.size(0)), key=lambda i: tensor[i])
 
 
-# Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
-# Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
-def GenerateSubgraphDict(subgraph_config: List) -> Dict:
-    subgraph_list = [GenerateSubgraphsWithSameEdgeTypeNum(
-        random_subgraph_num, subgraph_edge_type_num)
-        for random_subgraph_num, subgraph_edge_type_num
-        in subgraph_config]
-
-    return reduce(lambda x, y: {**x, **y}, subgraph_list)
-
-
-def Dict2List(dict: Dict) -> List:
-    return [(key, dict[key]) for key in dict]
-
-
-# Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
-# Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
-def GenerateSubgraphList(subgraph_config: List) -> List:
-    return Dict2List(GenerateSubgraphDict(subgraph_config))
-
-
-def OneTrialWithSubgraphList(subgraph_list: List, num_epochs: int) -> Tuple[
+def OneTrialWithSubgraphList(dataset, subgraph_list: List, num_epochs: int) -> Tuple[
         float, List, torch.Tensor]:
+
+    predict_class = dataset.TYPE_OF_NODE_TO_PREDICT
+    model = Fast_NARS_SGC_WithLearnableWeights(prop_steps=PROP_STEPS,
+                                               feat_dim=dataset.data.num_features[predict_class],
+                                               num_classes=dataset.data.num_classes[predict_class],
+                                               hidden_dim=HIDDEN_DIM, num_layers=NUM_LAYERS,
+                                               random_subgraph_num=len(subgraph_list))
+
+    device = torch.device(
+        f"cuda:{GpuWithMaxFreeMem()}" if torch.cuda.is_available() else "cpu")
+    classification = HeteroNodeClassification(dataset, predict_class, model,
+                                              lr=LR, weight_decay=WEIGHT_DECAY,
+                                              epochs=num_epochs, device=device,
+                                              train_batch_size=BATCH_SIZE,
+                                              eval_batch_size=BATCH_SIZE,
+                                              subgraph_list=subgraph_list)
+
+    test_acc = classification.test_acc
+    raw_weight = classification.subgraph_weight
+    weight_sum = raw_weight.sum()
+    normalized_weight = raw_weight/weight_sum
+
+    return test_acc, subgraph_list, normalized_weight
+
+
+# Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
+# Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
+def OneTrialWithSubgraphConfig(dataset, subgraph_config: List, num_epochs: int) -> Tuple[
+        float, List, torch.torch.Tensor]:
+    subgraph_list = GenerateSubgraphList(dataset, subgraph_config)
+    predict_class = dataset.TYPE_OF_NODE_TO_PREDICT
 
     model = Fast_NARS_SGC_WithLearnableWeights(prop_steps=PROP_STEPS,
                                                feat_dim=dataset.data.num_features[predict_class],
@@ -117,17 +126,16 @@ def OneTrialWithSubgraphList(subgraph_list: List, num_epochs: int) -> Tuple[
 
     device = torch.device(
         f"cuda:{GpuWithMaxFreeMem()}" if torch.cuda.is_available() else "cpu")
-    test_acc = HeteroNodeClassification(dataset, predict_class, model,
-                                        lr=LR, weight_decay=WEIGHT_DECAY,
-                                        epochs=num_epochs, device=device,
-                                        train_batch_size=BATCH_SIZE,
-                                        eval_batch_size=BATCH_SIZE,
-                                        subgraph_list=subgraph_list).test_acc
-
-    raw_weight = model.subgraph_weight
+    classification = HeteroNodeClassification(dataset, predict_class, model,
+                                              lr=LR, weight_decay=WEIGHT_DECAY,
+                                              epochs=num_epochs, device=device,
+                                              train_batch_size=BATCH_SIZE,
+                                              eval_batch_size=BATCH_SIZE,
+                                              subgraph_list=subgraph_list)
+    test_acc = classification.test_acc
+    raw_weight = classification.subgraph_weight
     weight_sum = raw_weight.sum()
     normalized_weight = raw_weight/weight_sum
-    print(normalized_weight)
 
     return test_acc, subgraph_list, normalized_weight
 
@@ -135,9 +143,10 @@ def OneTrialWithSubgraphList(subgraph_list: List, num_epochs: int) -> Tuple[
 # Input format: [(random_subgraph_num, subgraph_edge_type_num), ...]
 # Each element is a tuple of (random_subgraph_num, subgraph_edge_type_num)
 # Only top k subgraphs with highest weights are retained
-def OneTrialWithSubgraphListTopK(subgraph_config: List, k: int) -> float:
+def OneTrialWithSubgraphListTopK(dataset, subgraph_config: List, k: int,
+                                 num_epochs_to_find_weight: int, num_epochs_to_train: int) -> float:
     original_test_acc, subgraph_list, normalized_weight = OneTrialWithSubgraphConfig(
-        subgraph_config, SMALL_NUM_EPOCHS)
+        dataset, subgraph_config, num_epochs_to_find_weight)
     if k < len(subgraph_list):
         k = len(subgraph_list)
         warnings.warn('k is larger than the number of subgraphs,'
@@ -148,11 +157,21 @@ def OneTrialWithSubgraphListTopK(subgraph_config: List, k: int) -> float:
     retained_subgraph_list = [subgraph_list[i] for i in top_k_index]
 
     test_acc, _, _ = OneTrialWithSubgraphList(
-        retained_subgraph_list, NUM_EPOCHS)
+        dataset, retained_subgraph_list, num_epochs_to_train)
 
-    print('original_test_acc:', original_test_acc)
+    return test_acc, original_test_acc
+
+
+def main():
+    dataset = Dblp(root='.', path_of_zip='./dataset/DBLP_processed.zip')
+
+    test_acc, original_test_acc = OneTrialWithSubgraphListTopK(dataset,
+                                                               [(1, 2), (1, 3)], 2,
+                                                               SMALL_NUM_EPOCHS, NUM_EPOCHS)
+
     print('test_acc:', test_acc)
-    return test_acc
+    print('original_test_acc:', original_test_acc)
 
 
-#OneTrialWithSubgraphListTopK([(1, 1), (3, 2), (3, 3), (1, 4)], 3)
+if __name__ == '__main__':
+    main()
