c61b13e42b29c372a9f566f705aa6b01beac167f
Zheyu Lin <56953033+lzheyu@users.noreply.github.com>
2022-05-04 15:49:28 +0800

    Update search_models.py

--- sgl/search/search_models.py
+++ sgl/search/search_models.py
@@ -1,5 +1,5 @@
 from sgl.models.base_model import BaseSGAPModel
-from sgl.models.simple_models import LogisticRegression, MultiLayerPerceptron
+from sgl.models.simple_models import LogisticRegression, ResMultiLayerPerceptron
 from sgl.operators.graph_op import LaplacianGraphOp, PprGraphOp
 from sgl.operators.message_op import LastMessageOp, ConcatMessageOp, MeanMessageOp, SimpleWeightedMessageOp, \
     LearnableWeightedMessageOp, IterateLearnableWeightedMessageOp, SumMessageOp, MaxMessageOp, MinMessageOp
@@ -16,10 +16,14 @@ class SearchModel(BaseSGAPModel):
         pmsg_types = arch[6]
         super(SearchModel, self).__init__(prop_steps, feat_dim, num_classes)
 
-        if prop_types == 0:
+        if prop_types == 1:
             self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
-        else:
-            self._pre_graph_op = PprGraphOp(prop_steps, r=0.5, alpha=0.15)
+        elif prop_types == 2:
+            self._pre_graph_op = PprGraphOp(prop_steps, r=0.5, alpha=0.1)
+        elif prop_types == 3:
+            self._pre_graph_op = PprGraphOp(prop_steps, r=0.5, alpha=0.2)
+        elif prop_types == 4:
+            self._pre_graph_op = PprGraphOp(prop_steps, r=0.5, alpha=0.3)
 
         if mesg_types == 0:
             self._pre_msg_op = LastMessageOp()
@@ -37,20 +41,24 @@ class SearchModel(BaseSGAPModel):
         elif mesg_types == 6:
             self._pre_msg_op = SimpleWeightedMessageOp(0, prop_steps + 1, "alpha", 0.85)
         elif mesg_types == 7:
-            self._pre_msg_op = LearnableWeightedMessageOp(0, prop_steps + 1, "gate", feat_dim)
+            self._pre_msg_op = LearnableWeightedMessageOp(1, prop_steps + 1, "gate", feat_dim)
         elif mesg_types == 8:
-            self._pre_msg_op = IterateLearnableWeightedMessageOp(0, prop_steps + 1, "recursive", feat_dim)
+            self._pre_msg_op = LearnableWeightedMessageOp(1, prop_steps + 1, "simple", feat_dim)
 
         if num_layers == 1:
             self._base_model = LogisticRegression(feat_dim, num_classes)
         else:
-            self._base_model = MultiLayerPerceptron(feat_dim, hidden_dim, num_layers, num_classes)
+            self._base_model = ResMultiLayerPerceptron(feat_dim, hidden_dim, num_layers, num_classes)
 
         if post_types != 0 and post_steps != 0:
             if post_types == 1:
                 self._post_graph_op = LaplacianGraphOp(post_steps, r=0.5)
-            else:
-                self._post_graph_op = PprGraphOp(post_steps, r=0.5, alpha=0.15)
+            elif post_types == 2:
+                self._post_graph_op = PprGraphOp(post_steps, r=0.5, alpha=0.1)
+            elif post_types == 3:
+                self._post_graph_op = PprGraphOp(post_steps, r=0.5, alpha=0.2)
+            elif post_types == 4:
+                self._post_graph_op = PprGraphOp(post_steps, r=0.5, alpha=0.3)
 
             if pmsg_types == 0:
                 self._post_msg_op = LastMessageOp()
