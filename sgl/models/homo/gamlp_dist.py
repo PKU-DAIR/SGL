4c52009ee56f02699994cb49baad3096f665bcc6
Zheyu Lin <56953033+lzheyu@users.noreply.github.com>
2022-06-24 22:45:13 +0800

    Create gamlp_dist.py
    gamlp_dist model

---
+++ sgl/models/homo/gamlp_dist.py
@@ -0,0 +1,13 @@
+from sgl.models.base_model import BaseSGAPModelDist
+from sgl.models.simple_models import MultiLayerPerceptron
+from sgl.operators.graph_op import LaplacianGraphOp
+from sgl.operators.message_op import LearnableWeightedMessageOp
+
+
+class GAMLPDist(BaseSGAPModelDist):
+    def __init__(self, prop_steps, feat_dim, output_dim, hidden_dim, num_layers):
+        super(GAMLPDist, self).__init__(prop_steps, feat_dim, output_dim)
+
+        self._pre_graph_op = LaplacianGraphOp(prop_steps, r=0.5)
+        self._pre_msg_op = LearnableWeightedMessageOp(0, prop_steps + 1, "jk", prop_steps, feat_dim)
+        self._base_model = MultiLayerPerceptron(feat_dim, hidden_dim, num_layers, output_dim)
