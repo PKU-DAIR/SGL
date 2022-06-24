240480a748eecb8b316d908e66e8318d3430dd89
Zheyu Lin <56953033+lzheyu@users.noreply.github.com>
2022-06-24 22:54:44 +0800

    Update __init__.py

--- sgl/tasks/__init__.py
+++ sgl/tasks/__init__.py
@@ -6,6 +6,7 @@ from .link_prediction import LinkPredictionGAE
 from .link_prediction import LinkPredictionNAFS
 from .correct_and_smooth import NodeClassification_With_CorrectAndSmooth
 from .node_classification_with_label_use import NodeClassificationWithLabelUse
+from .node_classification_dist import NodeClassificationDist
 
 __all__ = [
     "NodeClassification",
@@ -14,6 +15,7 @@ __all__ = [
     "NodeClusteringNAFS",
     "LinkPredictionGAE",
     "LinkPredictionNAFS",
-    "NodeClassification_With_CorrectAndSmooth"
-    "NodeClassificationWithLabelUse"
+    "NodeClassification_With_CorrectAndSmooth",
+    "NodeClassificationWithLabelUse",
+    "NodeClassificationDist"
 ]
