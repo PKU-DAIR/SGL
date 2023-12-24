9b33e765c057cdf943a1facb77030ed27f159dfa
infinity <shensiqi1009@pku.edu.cn>
2023-12-24 14:01:46 +0000

    change search_config.py

--- examples/GDA/test_search_GAug.py
+++ examples/GDA/test_search_GAug.py
@@ -1,9 +1,10 @@
 import torch
 import argparse
+import torch.nn.functional as F
 from openbox import Optimizer
 
 import sgl.dataset as Dataset
-from sgl.search.gda_hpo.GAug_search_config import GAugOConfigManager, GAugMConfigManager
+from sgl.search.gda_hpo.search_config import BaseGDAConfigManager
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="HPO-GAug-Model.")
@@ -26,9 +27,28 @@ if __name__ == "__main__":
     pretrain_batch_size = args.pretrain_batch_size if args.pretrain_batch_size > 0 else None
     train_batch_size = args.train_batch_size if args.train_batch_size > 0 else None
     if args.model == "GAugO":
-        configer = GAugOConfigManager(dataset, args.gnn_type, not args.not_gae, device, minibatch=args.minibatch, pretrain_batch_size=pretrain_batch_size, train_batch_size=train_batch_size, runs=args.runs_per_config, max_patience=args.max_patience)
+        model_keys = ["in_dim", "hidden_dim", "emb_size", "n_classes", "n_layers", "dropout", "gnn_type", "activation", "temperature", "gae", "alpha", "feat_norm", "sample_type", "minibatch", "n_heads"]
+        task_keys = ["dataset", "model", "lr", "weight_decay", "epochs", "device", "beta", "warmup", "max_patience", "pretrain_ep", "pretrain_nc", "runs", "verbose", "seed", "pretrain_batch_size", "train_batch_size", "ep_lr"]
+        const_model_kwargs = dict(in_dim=dataset.num_features, n_classes=dataset.num_classes, gnn_type=args.gnn_type, activation=F.relu, gae=not args.not_gae, minibatch=args.minibatch, emb_size=32, n_layers=2, dropout=0.5, feat_norm="row")
+        const_task_kwargs = dict(dataset=dataset, epochs=200, device=device, max_patience=args.max_patience, pretrain_batch_size=pretrain_batch_size, train_batch_size=train_batch_size, runs=args.runs_per_config, verbose=False, lr=0.01, weight_decay=0.0005)
+        Reals = dict(alpha=dict(lower=0, upper=1, default_value=0.4, q=0.01), temperature=dict(lower=0.1, upper=2.1, default_value=1.5, q=0.1), beta=dict(lower=0, upper=4, default_value=2, q=0.1))
+        if pretrain_batch_size is not None:
+            Reals.update(ep_lr=dict(lower=0.001, upper=0.01, default_value=0.002, q=0.001))
+        else:
+            const_task_kwargs.update(ep_lr=0.01)
+        Categoricals = dict(hidden_dim=dict(choices=[32, 64, 128, 256], default_value=128))
+        Ints = dict(warmup=dict(lower=0, upper=10, default_value=2, q=1), pretrain_ep=dict(lower=5, upper=300, default_value=100, q=5), pretrain_nc=dict(lower=5, upper=300, default_value=100, q=5))
+        hier_params = dict(Real=Reals, Categorical=Categoricals, Int=Ints)
+        configer = BaseGDAConfigManager(args.model, f"NodeClassification{args.model}", model_keys, task_keys, const_model_kwargs, const_task_kwargs, hier_params)
     else:
-        configer = GAugMConfigManager(dataset, args.gnn_type, not args.not_gae, device, args.num_logits, runs=args.runs_per_config, max_patience=args.max_patience)
+        model_keys = ["in_dim", "hidden_dim", "n_classes", "n_layers", "gnn_type", "rm_pct", "add_pct", "choose_idx", "gae", "dropout", "activation", "feat_norm", "n_heads"]
+        task_keys = ["dataset", "model", "lr", "weight_decay", "epochs", "device", "max_patience", "runs", "verbose", "seed"]
+        const_model_kwargs = dict(in_dim=dataset.num_features, n_classes=dataset.num_classes, gnn_type=args.gnn_type, activation=F.relu, gae=not args.not_gae, n_layers=2, dropout=0.5, feat_norm="row")
+        const_task_kwargs = dict(dataset=dataset, epochs=200, device=device, max_patience=args.max_patience, runs=args.runs_per_config, verbose=False, lr=0.01, weight_decay=0.0005)
+        Categoricals = dict(hidden_dim=dict(choices=[32, 64, 128, 256], default_value=128))
+        Ints = dict(choose_idx=dict(lower=1, upper=args.num_logits, default_value=1, q=1), rm_pct=dict(lower=0, upper=80, default_value=20, q=1), add_pct=dict(lower=0, upper=80, default_value=20, q=1))
+        hier_params = dict(Categorical=Categoricals, Int=Ints)
+        configer = BaseGDAConfigManager(args.model, f"NodeClassification{args.model}", model_keys, task_keys, const_model_kwargs, const_task_kwargs, hier_params)
 
     opt = Optimizer(configer._configFunction,
                     configer._configSpace(),
